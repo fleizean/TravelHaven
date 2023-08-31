9c85d17dbad5097d89545ed5f82971e1c06a3b6b
Enes <nsyagz@gmail.com>
2023-08-31 18:25:29 +0300

    some translations and additions have been made

---
+++ TravelHaven/ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelHaven.ViewComponents.Default
+{
+	public class _SliderPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
+
